290a6aca47ad5ae916ce9f5f7fabb26891a744ed
Dawid Witek <dwitekw@gmail.com>
2020-05-23 21:26:35 +0200

    Questions pdf fixes, parsed to new format of json

--- read_questions.py
+++ read_questions.py
@@ -11,9 +11,12 @@ from argparse import ArgumentParser
 # First if needed, you have to remove the front page with command
 # pdftk in.pdf cat 2-end output out.pdf
 
+different_footer_filenames = ['2007_adw', '2007_not', '2007_rad', '2012_kom', '2017_kom']
+
 even_footer_pattern = '\s*\d+\s*EGZAMIN WSTĘPNY DLA KANDYDATÓW .+?(?=\d)'
 odd_footer_pattern = '\s*EGZAMIN WSTĘPNY DLA KANDYDATÓW .+?(?=\d)\d+\s*'
-question_number_pattern = '\d+[.]'
+different_even_footer_pattern = '\s*\d+\s*EGZAMIN KONKURSOWY DLA KANDYDATÓW .+?(?=\d)'
+different_odd_footer_pattern = '\s*EGZAMIN KONKURSOWY DLA KANDYDATÓW .+?(?=\d)\d+\s*'
 only_question_pattern = '\d+. (.+?(?=A))'
 first_answer_pattern = 'A[.].+?(?=B)'
 second_answer_pattern = 'B[.].+?(?=C)'
@@ -26,9 +29,7 @@ def extract_text_from_pdf(pdf_path):
     converter = TextConverter(resource_manager, fake_file_handle)
     page_interpreter = PDFPageInterpreter(resource_manager, converter)
     with open(pdf_path, 'rb') as fh:
-        for page in PDFPage.get_pages(fh,
-                                      caching=True,
-                                      check_extractable=True):
+        for page in PDFPage.get_pages(fh, caching=True, check_extractable=True):
             page_interpreter.process_page(page)
         text = fake_file_handle.getvalue()
     # close open handles
@@ -39,13 +40,17 @@ def extract_text_from_pdf(pdf_path):
 
 
 def parse_questions_to_json(path_to_file, output_directory, output_filename):
-    print(path_to_file)
+    filename_without_extension = output_filename.replace(".json", "")
 
     parsed_text = extract_text_from_pdf(path_to_file)
-    parsed_text = re.sub(even_footer_pattern, '', parsed_text)
-    parsed_text = re.sub(odd_footer_pattern, '', parsed_text)
-    # print(parsed_text)
-    parsed_json = {}
+    if filename_without_extension in different_footer_filenames:
+        parsed_text = re.sub(different_even_footer_pattern, '', parsed_text)
+        parsed_text = re.sub(different_odd_footer_pattern, '', parsed_text)
+    else:
+        parsed_text = re.sub(even_footer_pattern, '', parsed_text)
+        parsed_text = re.sub(odd_footer_pattern, '', parsed_text)
+
+    parsed_json = []
 
     current_number = 1
     next_number = 2
@@ -58,13 +63,7 @@ def parse_questions_to_json(path_to_file, output_directory, output_filename):
             is_not_finished = False
         else:
             full_question = full_question_match.group()
-            question_number = re.search(question_number_pattern, full_question).group()
             only_question = re.search(only_question_pattern, full_question).group(1)
-            print("--------------")
-            print("Full question " + full_question)
-            print("Question number " + question_number)
-            print("Only question " + only_question)
-
             first_answer = re.search(first_answer_pattern, full_question).group()
             first_answer = re.sub("A. ", '', first_answer)
             second_answer = re.search(second_answer_pattern, full_question).group()
@@ -72,33 +71,13 @@ def parse_questions_to_json(path_to_file, output_directory, output_filename):
             third_answer = re.search(third_answer_pattern, full_question).group()
             third_answer = re.sub("C. ", '', third_answer)
 
-            current_index = 'question_' + str(current_number)
-            question_json = {'question_number': question_number, 'question': only_question,
-                             'first_answer': first_answer,
-                             'second_answer': second_answer, 'third_answer': third_answer}
+            answers = {'A': first_answer, 'B': second_answer, 'C': third_answer}
+            question = {'question': only_question, 'answers': answers}
 
-            parsed_json[current_index] = question_json
+            parsed_json.append(question)
 
             current_number = next_number
             next_number += 1
-
-    # for index, full_question_match in enumerate(re.finditer(f"{current_number}\. (.+?(?={next_number}\.|$))", parsed_text, re.S)):
-    #     full_question = full_question_match.group()
-    #     question_number = re.search(question_number_pattern, full_question).group()
-    #     only_question = re.search(only_question_pattern, full_question).group(1)
-    #     first_answer = re.search(first_answer_pattern, full_question).group()
-    #     first_answer = re.sub("A. ", '', first_answer)
-    #     second_answer = re.search(second_answer_pattern, full_question).group()
-    #     second_answer = re.sub("B. ", '', second_answer)
-    #     third_answer = re.search(third_answer_pattern, full_question).group()
-    #     third_answer = re.sub("C. ", '', third_answer)
-    #
-    #     current_index = 'question_' + str(index + 1)
-    #     question_json = {'question_number': question_number, 'question': only_question, 'first_answer': first_answer,
-    #                      'second_answer': second_answer, 'third_answer': third_answer}
-    #
-    #     parsed_json[current_index] = question_json
-
 
     with open(os.path.abspath(os.path.join(output_directory, output_filename)), 'w+', encoding='utf-8') as file:
         json.dump(parsed_json, file, ensure_ascii=False, indent=4)
@@ -111,12 +90,7 @@ parser.add_argument("-o", "--output", dest="output", help="Output folder")
 if __name__ == '__main__':
     args = parser.parse_args()
 
-    print(args.input)
-    print(args.output)
-
     for directory_path, _, filenames in os.walk(args.input):
         for filename in filenames:
-            parse_questions_to_json(os.path.abspath(os.path.join(directory_path, filename)), args.output, filename.replace("pdf", "json"))
-
-    # for filename in os.listdir(args.input):
-    #     parse_questions_to_json(filename)
+            parse_questions_to_json(os.path.abspath(os.path.join(directory_path, filename)), args.output,
+                                    filename.replace("pdf", "json"))
